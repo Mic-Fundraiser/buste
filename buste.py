d1265a89b8a31d518aaf36f71ab6b239bf4d96c0
Mic <65023985+Mic-Fundraiser@users.noreply.github.com>
2024-02-21 14:24:57 +0100

    Update buste.py

--- buste.py
+++ buste.py
@@ -59,6 +59,7 @@ def compress_files_to_zip(output_folder, zip_name):
 
 # Interfaccia utente Streamlit
 st.title("Generatore di buste singole con logo, mittente e destinatario")
+st.write("Usare questo tracciato di esempio in .csv: Cognome |	Nome |	Indirizzo |	CAP |	Città |	Provincia"
 
 uploaded_csv = st.file_uploader("Carica il file CSV con i dati degli indirizzi", type='csv')
 uploaded_logo = st.file_uploader("Carica il logo (opzionale)", type=['png', 'jpg', 'jpeg'])
